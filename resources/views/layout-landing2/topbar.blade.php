59b759aba9d43a11d9901ef7f6726f0e87fc8f3c
alif <alifadiawan2005@gmail.com>
2023-05-28 13:39:34 +0700

    fiks dropdown EU

--- resources/views/layout-landing2/topbar.blade.php
+++ resources/views/layout-landing2/topbar.blade.php
@@ -6,10 +6,10 @@
 
     <nav id="navbar" class="navbar navbar-expand">
         <ul class="navbar-nav">
-            
-            <li class="nav-item"><a class="nav-link" href="{{route('blog.index')}}">Blog</a></li>
+
+            <li class="nav-item"><a class="nav-link" href="{{ route('blog.index') }}">Blog</a></li>
             {{-- <li class="nav-item"><a class="nav-link" href="{{ route('transaksi.index') }}">Store</a></li> --}}
-            <li class="nav-item"><a class="nav-link" href="{{route('store.index')}}">Store</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ route('store.index') }}">Store</a></li>
             <li class="nav-item"><a class="nav-link" href="/services/all">Service</a></li>
             <li class="nav-item"><a class="nav-link" href="{{ route('portfolio.index') }}">Portofolio</a></li>
 
@@ -23,7 +23,7 @@
                 <li class="nav-item"><a class="nav-link" href="/groupchat">Chats</a></li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('cart.index') }}">
-                        @if($badge->isEmpty())
+                        @if ($badge->isEmpty())
                             <i class="fa-solid fa-cart-shopping"></i>
                         @else
                             <i class="fa-solid fa-cart-shopping"></i>
@@ -32,26 +32,17 @@
                     </a>
                 </li>
 
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        {{ auth()->user()->username }}
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        {{-- <li><a class="dropdown-item" href="{{ route('storeEU.edit_profile') }}">Edit Profile</a></li> --}}
+
+                <li class="dropdown"><a href="#"><span>{{ auth()->user()->username }}</span> <i class="bi bi-chevron-down"></i></a>
+                    <ul>
                         <li><a class="dropdown-item" href="{{ route('user.show', auth()->user()->id) }}">Profile</a>
-                        </li>
-                        <li>
-                            <hr class="dropdown-divider">
                         </li>
                         <li><a class="dropdown-item" href="{{ route('transaksi.history', auth()->user()->id) }}">Histori
                                 Transaksi</a>
                         <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        @if(auth()->user()->role->role == 'admin' || auth()->user()->role->role == 'admin')
+                            @if (auth()->user()->role->role == 'admin' || auth()->user()->role->role == 'admin')
                         <li>
-                            <a class="dropdown-item" href="{{route('dashboard.index')}}">Ke Dashboard Admin</a>
+                            <a class="dropdown-item" href="{{ route('dashboard.index') }}">Ke Dashboard Admin</a>
                         </li>
                         @endif
                         <li>
@@ -60,9 +51,37 @@
                             </a>
                         </li>
                     </ul>
-
+                    {{-- <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        {{ auth()->user()->username }}
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        {{-- <li><a class="dropdown-item" href="{{ route('storeEU.edit_profile') }}">Edit Profile</a></li> --}}
+                {{-- <li><a class="dropdown-item" href="{{ route('user.show', auth()->user()->id) }}">Profile</a>
                 </li>
-            @endauth
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="{{ route('transaksi.history', auth()->user()->id) }}">Histori
+                        Transaksi</a>
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
+                @if (auth()->user()->role->role == 'admin' || auth()->user()->role->role == 'admin')
+                    <li>
+                        <a class="dropdown-item" href="{{ route('dashboard.index') }}">Ke Dashboard Admin</a>
+                    </li>
+                @endif
+                <li>
+                    <a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#logoutModal">
+                        Logout
+                    </a>
+                </li>
+            </ul>
+
+            </li> --}}
+        @endauth
 
 
         </ul>
