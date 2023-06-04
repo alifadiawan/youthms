7abf0a4aa7ebc86858fa4f315a2c010e01299d37
alif <alifadiawan2005@gmail.com>
2023-06-04 12:26:01 +0700

    konek landing page ke be

--- resources/views/EU/portofolio/index.blade.php
+++ resources/views/EU/portofolio/index.blade.php
@@ -8,8 +8,8 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-5 align-items-center" data-aos="zoom-in" data-aos-delay="200">
-                    {{-- <img src="{{ asset('illustration/group-390.png') }}" class="img-fluid" alt=""> --}}
-                    <lottie-player src="https://assets1.lottiefiles.com/packages/lf20_d9wImAFTrS.json" data-aos="fade-down" id="porto-animation" background="transparent" speed="1" style="width: 100%" loop  autoplay></lottie-player>
+                    <img src="{{ asset('illustration/porto.gif') }}" class="img-fluid" alt="">
+                    {{-- <lottie-player src="https://assets1.lottiefiles.com/packages/lf20_d9wImAFTrS.json" data-aos="fade-down" id="porto-animation" background="transparent" speed="1" style="width: 100%" loop  autoplay></lottie-player> --}}
                 </div>
                 <div class="col-lg-7 d-flex flex-column justify-content-center pt-4 pt-lg-0" data-aos="fade-up"
                     data-aos-delay="200">
