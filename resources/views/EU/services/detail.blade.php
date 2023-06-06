aa311c9ce52dc07653ab8e6e0b1d09eff5bbfa1d
skdlyn <rafli.dwiferdiansyah01@gmail.com>
2023-06-06 15:05:55 +0700

    req steven

--- resources/views/EU/services/detail.blade.php
+++ resources/views/EU/services/detail.blade.php
@@ -2,13 +2,16 @@
 @section('content')
     <section class="detail-service" id="detail-service">
         <div class="container" >
-            <header class="section-header" style="margin-top: 50px" data-aos="fade-down" data-aos-duration="2000">
+             <a href="{{ route('services.index') }}" class="btn btn yms-blue" style="margin-top: 50px" data-aos="fade-down" data-aos-duration="2000">
+            <i class="fas fa-arrow-left"></i>
+        </a>
+            <header class="section-header" style="margin-top: 25px;" data-aos="fade-down" data-aos-duration="2000">
                 <h1 class="fw-bold text-capitalize">{{$produk->services->jenis_layanan->layanan}}</h1>
             </header>
             <div class="col-lg-12">
                 <div class="row">
-                    <div class="col-lg-6" data-aos="fade-right" data-aos-duration="2000">
-                        <img src="{{ asset('produk/'.$produk->foto) }}" alt="">
+                    <div class="col-lg-6" data-aos="fade-up" data-aos-duration="2000">
+                        <img src="{{ asset('produk/'.$produk->foto) }}" alt="" width="500px">
                     </div>
                     <div class="col-lg-6" data-aos="fade-left" data-aos-duration="2000">
                         <h2 class="text-center">
