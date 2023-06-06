bc89a9d85b75d2745cec5079a65bb45af70d1a6b
alif <alifadiawan2005@gmail.com>
2023-06-06 17:28:57 +0700

    view paket admin

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -129,6 +129,14 @@
                             </a>
                         </li>
                         <li class="nav-item">
+                            <a href="/paket" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>
+                                    Paket
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
                             <a href="{{ route('pembayaran.list') }}" class="nav-link">
 
                                 <i class="far fa-circle nav-icon"></i>
