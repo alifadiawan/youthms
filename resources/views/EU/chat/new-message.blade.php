fd255eeb0894354846d3d35f617d152dbcfdb34d
Steven Alden <88414755+stevena1304@users.noreply.github.com>
2023-07-09 23:26:11 +0700

    fix notif n gc pt.2

--- resources/views/EU/chat/new-message.blade.php
+++ resources/views/EU/chat/new-message.blade.php
@@ -1,23 +1,23 @@
 @foreach ($messages as $message)
     @if ($message->user_id == auth()->user()->id)
         <div class="row justify-content-end mb-4">
-            <span class="text-end">{{ auth()->user()->username }}</span>
             <div class="col-4 mt-2">
-                <div class="p-3 border" style="border-radius: 10px; background-color: #406cab;">
-                    <p class=" mb-0 text-white text-end">{{ $message->message }}</p>
+                <div class="p-3 border" style="border-radius: 15px; background-color: #406cab;">
+                    <span class="text-start" style="color: #e4e4e4; font-weight:bold">{{ auth()->user()->username }}</span>
+                    <p class=" mb-0 text-white text-start">{{ $message->message }}</p>
                 </div>
-                <span class="time text-end text-muted">{{ $message->created_at->format('h:i A') }}</span>
+                <span class="time text-start text-muted">{{ $message->created_at->format('h:i A') }}</span>
             </div>
         </div>
     @else
-    <div class="row justify-content-start mb-4">
-        <span class="text-start">{{ $message->user->username }}</span>
-        <div class="col-4">
-            <div class="p-3 border" style="border-radius: 10px; background-color: #e4e4e4;">
-                <p class=" mb-0">{{ $message->message }}</p>
+        <div class="row justify-content-start mb-4">
+            <div class="col-4 mt-2">
+                <div class="p-3 border" style="border-radius: 15px; background-color: #e4e4e4;">
+                    <span class="text-start" style="color: black; font-weight:bold">{{ $message->user->username }}</span>
+                    <p class=" mb-0">{{ $message->message }}</p>
+                </div>
+                <span class="time text-start text-muted">{{ $message->created_at->format('h:i A') }}</span>
             </div>
-            <span class="time text-start text-muted">{{ $message->created_at->format('h:i A') }}</span>
         </div>
-    </div>
     @endif
 @endforeach
