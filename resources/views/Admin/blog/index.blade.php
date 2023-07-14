c8128f960c1498eb443a693af1a5a0a387da4eed
Steven Alden <88414755+stevena1304@users.noreply.github.com>
2023-07-15 01:23:13 +0700

    minor fix, filter, edit desc jenis layanan

--- resources/views/Admin/blog/index.blade.php
+++ resources/views/Admin/blog/index.blade.php
@@ -5,60 +5,92 @@
 <div class="row">
     <div class="col-lg-9">
         <div class="card p-3">
-            @if (count($segmen) > 0)
-                <a href="{{ route('blogs.create') }}" class="btn btn-md text-white rounded mb-2 mr-1"
-                    style="background-color: #1864BA;">Tambah Artikel</a>
-            @else
-                <a href="{{ route('blogs.create') }}" class="btn btn-md text-white rounded mb-2 mr-1 disabled"
-                    style="background-color: #1864BA;">Tambah Artikel</a>
-            @endif
-            <div class="table-responsive">
+            <div class="card-body">
+                @if (count($segmen) > 0)
+                    <a href="{{ route('blogs.create') }}" class="btn btn-md text-white rounded mb-2 mr-1"
+                        style="background-color: #1864BA;">Tambah Artikel</a>
+                @else
+                    <a href="{{ route('blogs.create') }}" class="btn btn-md text-white rounded mb-2 mr-1 disabled"
+                        style="background-color: #1864BA;">Tambah Artikel</a>
+                @endif
+                <div class="row justify-content-start align-items-center ps-1 pb-2">
+                    <label>Sort by :</label>
+                </div>
+                <div class="row justify-content-start align-items-center gap-2 gap-lg-0">
+                    <div class="col-12 col-lg-3">
+                        <div class="form-group">
+                            <div class="input-group">
+                                <input type="text" value="" placeholder="Tanggal" readonly class="form-control"
+                                    pattern="\d{4}-\d{2}-\d{2}" name="sort_tanggal" id="sort_tanggal">
+                                <button class="btn btn-outline-secondary" id="clear_button"><i
+                                        class="fa-solid fa-rotate fa-lg"></i></button>
+                            </div>
+                        </div>
 
-                <table class="table table-striped table-hover mt-2" id="blog-container">
-                    <thead>
-                        <tr style="background-color: #0EA1E2">
-                            <th class="text-white">No</th>
-                            <th class="text-white">Tanggal</th>
-                            <th class="text-white">Judul</th>
-                            <th class="text-white">Author</th>
-                            <th class="text-white">Segmen</th>
-                            <th class="text-white">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @if (count($data) == 0)
-                            <tr>
-                                <td colspan="6" class="text-center">Belum Ada Artikel !!</td>
+                    </div>
+                    <div class="col-12 col-lg-3">
+                        <div class="form-group">
+                            <div class="input-group">
+                                <select name="sort_status" class="form-control form-select text-capitalize" id="sort_status">
+                                    <option value="">Semua</option>
+                                    @foreach($segmen as $ss)
+                                    <option class="text-capitalize" value="{{$ss->segmen}}">{{$ss->segmen}}</option>
+                                    @endforeach
+                                </select>
+                                <button onClick="window.location.reload();" class="btn btn-outline-secondary"><i
+                                        class="fa-solid fa-rotate fa-lg"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="table-responsive">
+
+                    <table class="table table-striped table-hover mt-2" id="blog-container">
+                        <thead>
+                            <tr style="background-color: #0EA1E2">
+                                <th class="text-white">No</th>
+                                <th class="text-white">Tanggal</th>
+                                <th class="text-white">Judul</th>
+                                <th class="text-white">Author</th>
+                                <th class="text-white">Segmen</th>
+                                <th class="text-white">Action</th>
                             </tr>
-                        @else
-                            @foreach ($data as $i)
+                        </thead>
+                        <tbody>
+                            @if (count($data) == 0)
                                 <tr>
-                                    <td scope="row">{{ $loop->iteration }}</td>
-                                    <td>{{ $i->tanggal }}</td>
-                                    <td>{{ $i->judul }}</td>
-                                    <td>{{ $i->users->username }}</td>
-                                    <td>{{ $i->segmen->segmen }}</td>
-                                    <td>
-                                        <a href="{{ route('blogs.show', $i->id) }}"
-                                            class="btn btn-sm btn text-white rounded-pill"
-                                            style="background-color: #0EA1E2">Detail</a>
-                                    </td>
+                                    <td colspan="6" class="text-center">Belum Ada Artikel !!</td>
                                 </tr>
-                            @endforeach
-                        @endif
-                    </tbody>
-                </table>
-            </div>
+                            @else
+                                @foreach ($data as $i)
+                                    <tr class="main-row">
+                                        <td scope="row">{{ $loop->iteration }}</td>
+                                        <td>{{ date('d F Y', strtotime($i->created_at)) }}</td>
+                                        <td>{{ $i->judul }}</td>
+                                        <td>{{ $i->users->username }}</td>
+                                        <td class="text-capitalize">{{ $i->segmen->segmen }}</td>
+                                        <td>
+                                            <a href="{{ route('blogs.show', $i->id) }}"
+                                                class="btn btn-sm btn text-white rounded-pill"
+                                                style="background-color: #0EA1E2">Detail</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @endif
+                        </tbody>
+                    </table>
+                </div>
 
-            <div class="row" id="blog-pagination">
-                <nav>
-                    <ul class="pagination">
-                        @for ($page = 1; $page <= $data->lastPage(); $page++)
-                            <li class="page-item"><a class="page-link"
-                                    href="{{ $data->url($page) }}">{{ $page }}</a></li>
-                        @endfor
-                    </ul>
-                </nav>
+                <div class="row" id="blog-pagination">
+                    <nav>
+                        <ul class="pagination">
+                            @for ($page = 1; $page <= $data->lastPage(); $page++)
+                                <li class="page-item"><a class="page-link"
+                                        href="{{ $data->url($page) }}">{{ $page }}</a></li>
+                            @endfor
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </div>
     </div>
@@ -81,7 +113,7 @@
                     @else
                         @foreach ($segmen as $s)
                             <tr>
-                                <td>{{ $s->segmen }}</td>
+                                <td class="text-capitalize">{{ $s->segmen }}</td>
                                 <td><button data-toggle="modal" data-target="#hapusSegmen{{ $s->id }}"
                                         class="btn text-danger"><i class="fas fa fa-trash"></i></button></td>
                             </tr>
@@ -137,8 +169,53 @@
     </div>
 @endforeach
 
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
+    flatpickr("#sort_tanggal", {
+        dateFormat: "d F Y"
+    });
+
+    $(document).ready(function() {
+        $('#sort_status').on('change', function() {
+            filterData();
+        });
+
+        $('#sort_tanggal').on('change', function() {
+            filterData();
+        });
+
+        $('#clear_button').on('click', function() {
+            $('#sort_tanggal').val('');
+            filterData();
+        });
+    })
+
+    function filterData() {
+        var statusFilter = $('#sort_status').val().toLowerCase();
+        var tanggalFilter = $('#sort_tanggal').val().toLowerCase();
+
+        $('.main-row').hide();
+        if (statusFilter === 'all') {
+            $('.main-row').show();
+        }
+        $('.main-row').filter(function() {
+            var status = $(this).find('td:eq(4)').text().toLowerCase();
+            var tanggal = $(this).find('td:eq(1)').text().toLowerCase();
+
+            var matchesStatusFilter = status.includes(statusFilter);
+            var matchesTanggalFilter = true; // Default true jika filter tanggal kosong
+
+            if (tanggalFilter !== "Tanggal") {
+                matchesTanggalFilter = tanggal.includes(tanggalFilter);
+            }
+
+            return matchesStatusFilter && matchesTanggalFilter;
+        }).show();
+    }
+
+
     $('#blog-pagination').on('click', '.pagination a', function(event) {
         event.preventDefault();
 
