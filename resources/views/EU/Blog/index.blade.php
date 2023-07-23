f31bf7196480561cbe59016021c3b7fa201f6bc3
skdlyn <rafli.dwiferdiansyah01@gmail.com>
2023-07-23 09:52:19 +0700

    overflow

--- resources/views/EU/Blog/index.blade.php
+++ resources/views/EU/Blog/index.blade.php
@@ -15,7 +15,7 @@
                     <div class="row mt-40">
                         <div class="col-lg-8">
                             <div class="accordion accordion-flush" id="accordionFlushExample">
-                                <div class="accordion-item">
+                                <div class="accordion-item" >
                                     <h2 class="accordion-header" id="flush-headingOne">
                                         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                             data-bs-target="#flush-collapseOne" aria-expanded="false"
@@ -24,8 +24,9 @@
                                         </button>
                                     </h2>
                                     <hr>
+                                    <div class="overflow-auto" style="max-height: 575px;">
                                     @foreach ($trending as $b)
-                                        <div id="flush-collapseOne" class="accordion-collapse collapse"
+                                        <div id="flush-collapseOne" class="accordion-collapse collaps"
                                             aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
                                             <div class="trending-top mb-30">
                                                 <div class="trend-top-img">
@@ -44,6 +45,7 @@
                                             </div>
                                         </div>
                                     @endforeach
+                                    </div>
                                 </div>
                             </div>
                             <!-- Trending Top -->
